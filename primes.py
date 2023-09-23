ca4333cd4f86cae31f66b4684ec9bf9ad8be6183
Aaliyah Merchant <aaliyah.merchant@kcl.ac.uk>
2023-09-23 18:34:53 +0100

    Complete primes 2 exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if (number_of_primes <= 0):
+        raise ValueError(f"The number_of_primes {number_of_primes} was negative.")
+
     list = []
+
+    curr = 2
+
+    while (len(list) < number_of_primes):
+        prime = True
+
+        for i in range(2, curr):
+            if (curr % i == 0):
+                prime = False
+                break
+        
+        if (prime):
+            list.append(curr)
+
+        curr += 1
+
     return list
